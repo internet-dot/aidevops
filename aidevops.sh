605bd1bbed1b5622284e3e7ee1c874608c021695
marcusquinn <6428977+marcusquinn@users.noreply.github.com>
2026-03-30 06:24:12 +0100

    chore: bump version to v3.5.421

--- aidevops.sh
+++ aidevops.sh
@@ -4,9 +4,9 @@
 # Usage: aidevops <command> [options]
 #
 <<<<<<< Updated upstream
-# Version: 3.5.420
+# Version: 3.5.421
 =======
-# Version: 3.5.420
+# Version: 3.5.421
 >>>>>>> Stashed changes
 
 set -euo pipefail
